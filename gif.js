40ca15df1bb634169747458c064fd440768bb51e
nateodes <nateodes@gmail.com>
2017-06-17 19:59:40 -0600

    css updates

--- gif.js
+++ gif.js
@@ -3,29 +3,23 @@
 // The public beta key is "dc6zaTOxFJmzC”
 
 
-var topics = ["baseball", "baskeball", "football", "hockey", "olympics", "golf", "curling"]
+var topics = ["Kenny Powers", "South Park", "baseball", "baskeball", "football", "hockey", "olympics", "golf", "curling"]
 
 $(".gifSearch").on("click", function(event) {
         event.preventDefault();
-        // This line grabs the input from the textbox
+        
         var newTopic = $("#search").val().trim();
-        // var newButton = $("<button>");
-        // newButton.addClass("btn-sm");
-        // newButton.html(newTopic);
-        // newButton.attr({ "data-name": newTopic });
+        
         topics.push(newTopic);
-        // Calling renderButtons which handles the processing of our movie array
         renderButtons();
       });
 
 function renderButtons() {
-        // Deleting the movie buttons prior to adding new movie buttons
-        // (this is necessary otherwise we will have repeat buttons)
+        
         $(".gifButtons").empty();
-        // Looping through the array of movies
         for (var i = 0; i < topics.length; i++) {
           var gifButton = $("<button>");
-              gifButton.addClass("btn-sm");
+              gifButton.addClass("btn-sm btn-d btn-info");
               gifButton.attr({ "data-name": topics[i] });
               gifButton.html(topics[i]);
               $(".gifButtons").append(gifButton);
@@ -42,20 +36,13 @@ renderButtons();
 
 
 
-// for (var i = 0; i < topics.length; i++) {
-//             var gifButton = $("<button>");
-//               gifButton.addClass("btn-sm");
-//               gifButton.attr({ "data-name": topics[i] });
-//               gifButton.html(topics[i]);
-//               $(".gifButtons").append(gifButton);
-//           }
-// Set Functions--------------------------------------------------
+
 
 $(document.body).on("click", ".btn-sm", function() {
 		var searchGif = ($(this).attr("data-name"));
 		// var newGif = $("#email").val().trim();
         // URL
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchGif + "&api_key=dc6zaTOxFJmzC";  
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchGif + "&limit=10&rating=pg&rating=g&sort=recent&api_key=dc6zaTOxFJmzC";  
         
         $.ajax({
       url: queryURL,
@@ -64,14 +51,15 @@ $(document.body).on("click", ".btn-sm", function() {
         console.log(response.data[0].embed_url);
         var results = response.data;
           for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='item'>");
+            var gifDiv = $("<div class='item'><span class='rating'>");
             var rating = results[i].rating;
-            var p = $("<p>").text("Rating: " + rating);
+            // var p = $("<span class='rating'>").text("Rating: " + rating);
             var personImage = $("<img class='gif'>");
             
             personImage.attr("data-still", results[i].images.original_still.url);
-            gifDiv.prepend(p);
+            // $(".rating").text(rating);
             gifDiv.prepend(personImage);
+            // $(".newDiv").append(p);
             $("#gifBin").prepend(gifDiv);
             // personImage.attr("data-state", "still");
             // gifDiv.attr("data-state", "still");
